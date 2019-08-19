799afea5d2138da78f93167c6ef403a558837297
Mike Watkins <python@mikewatkins.ca>
2019-08-19 16:21:40 -0700

    add package declaration

--- bolthold/doc.go
+++ bolthold/doc.go
@@ -1,2 +1,5 @@
-// Package bolthold provides a BoltHold-backed implementation of the hoser.UserService interface.
+/*
+Package bolthold provides a BoltHold-backed implementation of the hoser.UserService interface.
+Package boltholdand an HTTP client for the UserService suitable for incorporating within web applications.
+*/
 package bolthold
--- doc.go
+++ doc.go
@@ -2,8 +2,6 @@
 Package hoser provides a basic User concept incorporating a simple
 role/permissions capability; a UserService interface to manage users and
 a concrete implementation backed by BoltHold, a wrapper around the capable
-BoltDB; and an HTTP client for the UserService suitable for incorporating
-within web applications.
-
+BoltDB.
 */
 package hoser
